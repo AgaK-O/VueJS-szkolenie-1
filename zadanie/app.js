a8486621c9134591a1eaaaf2251d7e75cda4d19f
Agnieszka Krzyszton <akrzyszton@divante.pl>
2017-12-15 10:33:42 +0100

    poprawki

--- zadanie/app.js
+++ zadanie/app.js
@@ -3,10 +3,10 @@ var app = new Vue({
     data: {
         pageName: 'Atlas zwierząt',
         animals: [
-            { name: 'Lew', imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/7/73/Lion_waiting_in_Namibia.jpg', even: false },
-            { name: 'Kot', imgSrc: 'http://www.catster.com/wp-content/uploads/2017/06/small-kitten-meowing.jpg', even: true },
-            { name: 'Pies', imgSrc: 'https://s7d2.scene7.com/is/image/PetSmart/PB1201_STORY_CARO-Authority-HealthyOutside-DOG-20160818?$PB1201$', even: false},
-            { name: 'Zebra', imgSrc: 'https://a-z-animals.com/media/animals/images/470x370/zebra_8.jpg', even: true }
+            { name: 'Lew', imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/7/73/Lion_waiting_in_Namibia.jpg' },
+            { name: 'Kot', imgSrc: 'http://www.catster.com/wp-content/uploads/2017/06/small-kitten-meowing.jpg' },
+            { name: 'Pies', imgSrc: 'https://s7d2.scene7.com/is/image/PetSmart/PB1201_STORY_CARO-Authority-HealthyOutside-DOG-20160818?$PB1201$' },
+            { name: 'Zebra', imgSrc: 'https://a-z-animals.com/media/animals/images/470x370/zebra_8.jpg' }
         ]
     },
     methods: {
